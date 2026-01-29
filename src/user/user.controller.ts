017f3e70f01ebf8b2bc9b25e66d7f2caa4b5ed2a
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-29 15:01:11 +0545

    feat(users/controller): add UserController with create endpoint

---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './providers/user.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('users')
+@Controller('users')
+export class UserController {
+  constructor(private readonly UserService: UserService) {}
+
+  @Post()
+  create(@Body() body: CreateUserDto) {
+    return this.UserService.create(
+      body.username,
+      body.email,
+      body.password,
+      body.avatar,
+    );
+  }
+}
