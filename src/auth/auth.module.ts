abcb68a482661f1688c2b828091acc8a67290565
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-30 11:14:23 +0545

    feat(auth): add JWT access and refresh token strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,19 @@ import { HashingProvider } from './providers/hashing.provider';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
-import { LocalStrategy } from './strategies/local.startegy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from 'src/configs/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtAccessTokenStrategy } from './strategies/jwt-access.strategy';
+import { JwtRefreshTokenStrategy } from './strategies/jwt-refresh.strategy';
 
 @Module({
-  imports: [forwardRef(() => UserModule)],
+  imports: [
+    forwardRef(() => UserModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule,
+  ],
   exports: [HashingProvider],
   providers: [
     AuthService,
@@ -17,6 +26,8 @@ import { LocalStrategy } from './strategies/local.startegy';
     },
     BcryptProvider,
     LocalStrategy,
+    JwtAccessTokenStrategy,
+    JwtRefreshTokenStrategy,
   ],
   controllers: [AuthController],
 })
