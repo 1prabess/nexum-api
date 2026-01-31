d256c7c2e6e68d26bae3c3b8d899bbafe3e7e6bf
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-31 16:18:10 +0545

    feat(user): add User profile fields, follower relations

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,9 +1,11 @@
+import { Follow } from 'src/follow/follow.entity';
 import {
   Column,
   Entity,
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm';
 
 @Entity('users')
@@ -14,6 +16,18 @@ export class User {
   @Column({ type: 'varchar', length: 50, unique: true })
   username: string;
 
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  fullName: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  bio?: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  coverPhoto?: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  avatar?: string;
+
   @Column({ type: 'varchar', length: 100, unique: true })
   email: string;
 
@@ -21,10 +35,19 @@ export class User {
   password: string;
 
   @Column({ type: 'varchar', length: 255, nullable: true })
-  avatar?: string;
-
-  @Column({ type: 'varchar', length: 255, nullable: true })
   refreshToken?: string;
+
+  @Column({ type: 'int', default: 0 })
+  followersCount: number;
+
+  @Column({ type: 'int', default: 0 })
+  followingCount: number;
+
+  @OneToMany(() => Follow, (follow) => follow.follower)
+  followers: Follow[];
+
+  @OneToMany(() => Follow, (follow) => follow.following)
+  following: Follow[];
 
   @CreateDateColumn({ type: 'timestamptz' })
   createdAt: Date;
