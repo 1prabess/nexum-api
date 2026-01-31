2e0767c65ead73ed406c4f3d65441a7d488e25de
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-31 16:25:15 +0545

    feat(auth): add user registration endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { ApiOperation, ApiBody, ApiCookieAuth } from '@nestjs/swagger';
 import { LoginUserDto } from './dtos/login-user.dto';
@@ -8,6 +8,8 @@ import { AuthService } from './providers/auth.service';
 import { setAuthCookies } from './utils/cookies.utils';
 import type { Response } from 'express';
 import { JwtRefreshTokenAuthGuard } from './guards/jwt-refresh-auth.guard';
+import { RegisterUserDto } from './dtos/register-user.dto';
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -16,30 +18,42 @@ export class AuthController {
   @ApiOperation({ summary: 'Login with email and password' })
   @ApiBody({ type: LoginUserDto })
   @Post('/login')
+  @ResponseMessage('Login successful')
   @UseGuards(LocalAuthGuard)
   async login(
     @CurrentUser() user: User,
     @Res({ passthrough: true }) response: Response,
-  ) {
+  ): Promise<void> {
     const { accessToken, refreshToken } = await this.authService.login(user);
 
     setAuthCookies(response, accessToken, refreshToken);
+  }
 
-    return { message: 'Login successful' };
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: RegisterUserDto })
+  @Post('/register')
+  @ResponseMessage('User registration successful')
+  async register(
+    @Body() registerUserDto: RegisterUserDto,
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<void> {
+    const { accessToken, refreshToken } =
+      await this.authService.register(registerUserDto);
+
+    setAuthCookies(response, accessToken, refreshToken);
   }
 
   @ApiOperation({ summary: 'Refresh access and refresh tokens' })
   @ApiCookieAuth('refreshToken')
   @Get('/refresh')
+  @ResponseMessage('Tokens refresh successful')
   @UseGuards(JwtRefreshTokenAuthGuard)
   async refreshToken(
     @CurrentUser() user: User,
     @Res({ passthrough: true }) response: Response,
-  ) {
+  ): Promise<void> {
     const { accessToken, refreshToken } = await this.authService.login(user);
 
     setAuthCookies(response, accessToken, refreshToken);
-
-    return { message: 'Tokens refreshed' };
   }
 }
