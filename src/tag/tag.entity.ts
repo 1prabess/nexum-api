63d7f39a53727e707da79805a6f2dbe1c91d7309
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-02-01 13:59:21 +0545

    feat(tag): create tag entity

---
+++ src/tag/tag.entity.ts
@@ -0,0 +1,23 @@
+import { Post } from 'src/post/post.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('tags')
+export class Tag {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  name: string;
+
+  @ManyToMany(() => Post, (post) => post.tags)
+  posts: Post[];
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+}
