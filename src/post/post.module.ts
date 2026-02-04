56c072d79b85d1b8d2ad1f20ea713de9dc2a8573
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-02-04 15:07:46 +0545

    feat(user): add post votes and comments relation to user entity

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,18 +2,20 @@ import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './providers/post.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from './post.entity';
+import { Post } from './entities/post.entity';
 import { UserModule } from 'src/user/user.module';
 import { TagModule } from 'src/tag/tag.module';
 import { CommunityModule } from 'src/community/community.module';
+import { PostVote } from './entities/post-vote.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Post]),
+    TypeOrmModule.forFeature([Post, PostVote]),
     UserModule,
     TagModule,
     CommunityModule,
   ],
+  exports: [PostService],
   controllers: [PostController],
   providers: [PostService],
 })
