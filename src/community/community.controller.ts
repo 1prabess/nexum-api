d489d52c55e641a9076997c1c490986070b5a98f
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-02-19 18:25:21 +0545

    feature(community): apply custom ApiSuccessResponse decorator for swagger doc

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -12,12 +12,15 @@ import {
 import { CreateCommunityDto } from './dtos/create-community.dto';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import type { ICurrentUser } from 'src/common/interfaces/current-user.interface';
-import { ApiOperation, ApiParam } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
 import { JwtAccessTokenAuthGuard } from 'src/auth/guards/jwt-access-auth.guard';
 import { UpdateCommunityDto } from './dtos/update-community.dto';
 import { CommunityService } from './services/community.service';
 import { CommunityInvite } from './entities/community-invite.entity';
+import { CommunityResponseDto } from './dtos/community-response.dto';
+import { CommunityInviteResponseDto } from './dtos/community-invite.response.dto';
+import { ApiSuccessResponse } from 'src/common/decorators/api-success-response.decorator';
 
 @Controller('communities')
 @UseGuards(JwtAccessTokenAuthGuard)
@@ -26,24 +29,32 @@ export class CommunityController {
 
   // Create community
   @ApiOperation({ summary: 'Create a new community' })
+  @ApiSuccessResponse(CommunityResponseDto, {
+    message: 'Community created successfully',
+  })
   @Post()
   @ResponseMessage('Community created successfully')
   create(
     @Body() createCommunityDto: CreateCommunityDto,
     @CurrentUser() user: ICurrentUser,
-  ) {
+  ): Promise<CommunityResponseDto> {
     return this.communityService.create(user, createCommunityDto);
   }
 
   // Update community
   @ApiOperation({ summary: 'Update a community (owner only)' })
+  @ApiResponse({
+    status: 200,
+    description: 'Community updated',
+    type: CommunityResponseDto,
+  })
   @Patch(':communityId')
   @ResponseMessage('Community updated successfully')
   update(
     @Param('communityId', ParseIntPipe) communityId: number,
     @Body() updateCommunityDto: UpdateCommunityDto,
     @CurrentUser() user: ICurrentUser,
-  ): Promise<void> {
+  ): Promise<CommunityResponseDto> {
     return this.communityService.update(user, communityId, updateCommunityDto);
   }
 
@@ -100,9 +111,15 @@ export class CommunityController {
 
   // Get  community invites
   @ApiOperation({ summary: 'Get my pending community invitations' })
+  @ApiSuccessResponse(CommunityInviteResponseDto, {
+    isArray: true,
+    message: 'Community invitations fetched successfully',
+  })
   @Get('invites')
   @ResponseMessage('Community invitations fetched successfully')
-  getMyInvites(@CurrentUser() user: ICurrentUser): Promise<CommunityInvite[]> {
+  getMyInvites(
+    @CurrentUser() user: ICurrentUser,
+  ): Promise<CommunityInviteResponseDto[]> {
     return this.communityService.getInvites(user.id);
   }
 }
