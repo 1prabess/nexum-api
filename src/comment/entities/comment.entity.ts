f37da1887299e6002c807627b2f6a89776fee7f8
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-04-06 15:14:51 +0545

    refactor(comment): update comment.entity.ts

--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,5 +1,7 @@
 import { Post } from 'src/post/entities/post.entity';
+import { Question } from 'src/question/entities/question.entity';
 import { User } from 'src/user/user.entity';
+import { CommentVote } from './comment-vote.entity';
 import {
   Column,
   CreateDateColumn,
@@ -28,6 +30,11 @@ export class Comment {
   })
   post: Post;
 
+  @ManyToOne(() => Question, (question) => question.comments, {
+    onDelete: 'CASCADE',
+  })
+  question: Question;
+
   @ManyToOne(() => Comment, (comment) => comment.replies, {
     nullable: true,
     onDelete: 'CASCADE',
@@ -36,6 +43,15 @@ export class Comment {
 
   @OneToMany(() => Comment, (comment) => comment.parent)
   replies: Comment[];
+
+  @OneToMany(() => CommentVote, (vote) => vote.comment)
+  votes: CommentVote[];
+
+  @Column({ type: 'int', default: 0 })
+  upvotes: number;
+
+  @Column({ type: 'int', default: 0 })
+  downvotes: number;
 
   @CreateDateColumn({ type: 'timestamptz' })
   createdAt: Date;
