235bb5dbb1aa6a3c873bcfe141d40b1e093df861
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-04-06 15:14:52 +0545

    refactor(post): update post.module.ts

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,19 +1,26 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PostController } from './post.controller';
-import { PostService } from './providers/post.service';
+import { PostService } from './post.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 import { UserModule } from 'src/user/user.module';
 import { TagModule } from 'src/tag/tag.module';
 import { CommunityModule } from 'src/community/community.module';
 import { PostVote } from './entities/post-vote.entity';
+import { SearchModule } from 'src/search/search.module';
+import { Tag } from 'src/tag/tag.entity';
+import { NotificationModule } from 'src/notification/notification.module';
+import { FollowModule } from 'src/follow/follow.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Post, PostVote]),
+    TypeOrmModule.forFeature([Post, PostVote, Tag]),
     UserModule,
     TagModule,
-    CommunityModule,
+    forwardRef(() => CommunityModule),
+    SearchModule,
+    NotificationModule,
+    FollowModule,
   ],
   exports: [PostService],
   controllers: [PostController],
