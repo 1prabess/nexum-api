51dda1302c23bc7c4a3c50dfaf968f9ece498a21
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-04-06 15:14:53 +0545

    refactor(user): update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,19 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { UserService } from './providers/user.service';
+import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserController } from './user.controller';
+import { FollowModule } from 'src/follow/follow.module';
+import { Community } from 'src/community/entities/community.entity';
+import { CommunityMember } from 'src/community/entities/community-member.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [
+    TypeOrmModule.forFeature([User, Community, CommunityMember]),
+    forwardRef(() => AuthModule),
+    forwardRef(() => FollowModule),
+  ],
   exports: [UserService],
   providers: [UserService],
   controllers: [UserController],
