4328f143ebf05333f156b82d5cd785405590c383
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-04-06 15:14:51 +0545

    refactor(auth): update register-user.dto.ts

--- src/auth/dtos/register-user.dto.ts
+++ src/auth/dtos/register-user.dto.ts
@@ -1,21 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
   @ApiProperty({
     description: 'Unique username for the user',
     example: 'john_doe',
   })
+  @IsString()
   username: string;
 
   @ApiProperty({
     description: 'Email address of the user',
     example: 'john@example.com',
   })
+  @IsEmail()
   email: string;
 
   @ApiProperty({
     description: 'Password for the user account',
     example: 'strongPassword123',
   })
+  @IsString()
+  @MinLength(6)
   password: string;
 }
