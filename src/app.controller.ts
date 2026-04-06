ca78e7c5042ab69e4a880067413752a1e4564683
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-04-06 15:14:51 +0545

    refactor(app.controller.ts): update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  // ------------------ HEALTH CHECK ------------------
   @Get('/healthcheck')
   healthCheck(): { status: string } {
     return { status: 'ok' };
