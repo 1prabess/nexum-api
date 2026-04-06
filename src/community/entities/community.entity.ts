f86804127c3a484d2f6f8305b0a2e5111a5c45d2
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-04-06 15:14:52 +0545

    refactor(community): update community.entity.ts

--- src/community/entities/community.entity.ts
+++ src/community/entities/community.entity.ts
@@ -6,11 +6,15 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
 import { CommunityVisibility } from '../enums/community-visibility.enum';
 import { User } from 'src/user/user.entity';
 import { CommunityMember } from './community-member.entity';
 import { Post } from 'src/post/entities/post.entity';
+import { Question } from 'src/question/entities/question.entity';
+import { Tag } from 'src/tag/tag.entity';
 
 @Entity('communities')
 export class Community {
@@ -44,6 +48,13 @@ export class Community {
 
   @OneToMany(() => Post, (post) => post.community)
   posts: Post[];
+
+  @OneToMany(() => Question, (question) => question.community)
+  questions: Question[];
+
+  @ManyToMany(() => Tag, (tag) => tag.communities)
+  @JoinTable()
+  tags: Tag[];
 
   @CreateDateColumn({ type: 'timestamptz' })
   createdAt: Date;
